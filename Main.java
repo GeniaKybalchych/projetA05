fa0e5e2f2fb880a67d5eaa162c5f8f9f964e24aa
rbabari <r.babari@gmail.com>
2022-10-31 16:37:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public static void main(args[])
   
   System.out.println("Hello!");
   System.out.println("Yehhhhhh Super !");
+  System.out.println("Yehhhhhh Super !");
 
 {
 MonApplication app = new MonApplication();
